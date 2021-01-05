506a8619fea4bd6c611ca772f2de3f5f8030796c
praguzz <pratamagusta@live.com>
2021-01-05 20:55:57 +0800

    test123

--- index.js
+++ index.js
@@ -38,7 +38,144 @@ function handleEvent(event) {
         request({url: "http://api.openweathermap.org/data/2.5/weather?q="+result.entities?.[0]?.sourceText.replace(" ", "%20")+"&appid=" + config.openWeatherSecret, method: "GET"}, (err, resp, body) => {
             let asd = JSON.parse(body);
             searchReply.text = JSON.stringify(asd);
-            return client.replyMessage(event.replyToken, searchReply).catch((error)=>{
+
+            let object = {
+                "type": "bubble",
+                "hero": {
+                  "type": "image",
+                  "url": "https://www.flaticon.com/svg/static/icons/svg/1247/1247109.svg",
+                  "size": "full",
+                  "aspectRatio": "20:13",
+                  "aspectMode": "cover",
+                  "action": {
+                    "type": "uri",
+                    "label": "Action",
+                    "uri": "https://linecorp.com/"
+                  }
+                },
+                "body": {
+                  "type": "box",
+                  "layout": "vertical",
+                  "spacing": "md",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "Weather in Jakarta",
+                      "weight": "bold",
+                      "size": "xl",
+                      "gravity": "center",
+                      "wrap": true,
+                      "contents": []
+                    },
+                    {
+                      "type": "box",
+                      "layout": "vertical",
+                      "spacing": "sm",
+                      "margin": "lg",
+                      "contents": [
+                        {
+                          "type": "box",
+                          "layout": "baseline",
+                          "spacing": "sm",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "Date",
+                              "size": "sm",
+                              "color": "#AAAAAA",
+                              "flex": 1,
+                              "contents": []
+                            },
+                            {
+                              "type": "text",
+                              "text": "Monday 25, 9:00PM",
+                              "size": "sm",
+                              "color": "#666666",
+                              "flex": 4,
+                              "wrap": true,
+                              "contents": []
+                            }
+                          ]
+                        },
+                        {
+                          "type": "box",
+                          "layout": "baseline",
+                          "spacing": "sm",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "Place",
+                              "size": "sm",
+                              "color": "#AAAAAA",
+                              "flex": 1,
+                              "contents": []
+                            },
+                            {
+                              "type": "text",
+                              "text": "7 Floor, No.3",
+                              "size": "sm",
+                              "color": "#666666",
+                              "flex": 4,
+                              "wrap": true,
+                              "contents": []
+                            }
+                          ]
+                        },
+                        {
+                          "type": "box",
+                          "layout": "baseline",
+                          "spacing": "sm",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "Seats",
+                              "size": "sm",
+                              "color": "#AAAAAA",
+                              "flex": 1,
+                              "contents": []
+                            },
+                            {
+                              "type": "text",
+                              "text": "C Row, 18 Seat",
+                              "size": "sm",
+                              "color": "#666666",
+                              "flex": 4,
+                              "wrap": true,
+                              "contents": []
+                            }
+                          ]
+                        }
+                      ]
+                    },
+                    {
+                      "type": "box",
+                      "layout": "vertical",
+                      "margin": "xxl",
+                      "contents": [
+                        {
+                          "type": "spacer"
+                        },
+                        {
+                          "type": "image",
+                          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/linecorp_code_withborder.png",
+                          "size": "xl",
+                          "aspectMode": "cover"
+                        },
+                        {
+                          "type": "text",
+                          "text": "You can enter the theater by using this code instead of a ticket",
+                          "size": "xs",
+                          "color": "#AAAAAA",
+                          "margin": "xxl",
+                          "wrap": true,
+                          "contents": []
+                        }
+                      ]
+                    }
+                  ]
+                }
+              };
+            return client.replyMessage(event.replyToken, object).catch((error)=>{
                 console.log(error)
             });
         })
